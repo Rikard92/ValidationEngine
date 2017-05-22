ff944f5741ef684fd1a46eac3e8f162fc09e7ea7
Rikard <Rikard.Persson92@hotmail.com>
2017-05-22 13:45:14 +0200

    Add project files.

---
+++ ValidationEngine/Validator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ValidationEngine
+{
+    public class Validator
+    {
+        public Validator()
+        {
+
+        }
+
+        public bool ValidateEmailAddress(string mail)
+        {
+            if (mail == null)
+            {
+                return false;
+            }
+
+            if (mail.Contains(".")&&mail.Contains("@"))
+            {
+            
+                string[] split1 = mail.Split('@');
+                string fh = split1[0];
+                string sh = split1[1];
+
+                if (fh.Contains(".") )
+                {
+                    return false;
+                }
+                if (fh.Any(char.IsDigit))
+                {
+                    return false;
+                }
+
+            if (sh.Any(char.IsDigit))
+            {
+                return false;
+            }
+
+            return true;
+            }
+            return false;
+        }
+
+        
+    }
+}
---
+++ ValidationEngineTests/ValidationTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ValidationEngine;
+
+namespace ValidationEngineTests
+{
+    [TestFixture]
+    public class ValidationTests
+    {
+        
+        [Test]
+        public void TrueForValidAddress()
+        {
+            //Arrange
+            var sut = new Validator();
+            string mail = "name@test.com";
+
+            //Act
+            bool res= sut.ValidateEmailAddress(mail);
+
+            //Assert
+            Assert.IsTrue(res, "The email is working.");
+
+        }
+
+        [Test]
+        public void TrueForValidAddress2()
+        {
+            //Arrange
+            var sut = new Validator();
+            string mail = "name@testcom";
+
+            //Act
+            bool res = sut.ValidateEmailAddress(mail);
+
+            //Assert
+            Assert.IsTrue(res, "The email is working.");
+
+        }
+
+        [Test]
+        public void TrueForValidAddress3()
+        {
+            //Arrange
+            var sut = new Validator();
+            string mail = "nametest.com";
+
+            //Act
+            bool res = sut.ValidateEmailAddress(mail);
+
+            //Assert
+            Assert.IsTrue(res, "The email is working.");
+
+        }
+
+        [Test]
+        public void TrueForValidAddress4()
+        {
+            //Arrange
+            var sut = new Validator();
+            string mail = "name123@test.com";
+
+            //Act
+            bool res = sut.ValidateEmailAddress(mail);
+
+            //Assert
+            Assert.IsTrue(res, "The email is working.");
+
+        }
+
+        [Test]
+        public void TrueForValidAddress5()
+        {
+            //Arrange
+            var sut = new Validator();
+            string mail = "name@test123.com";
+
+            //Act
+            bool res = sut.ValidateEmailAddress(mail);
+
+            //Assert
+            Assert.IsTrue(res, "The email is working.");
+
+        }
+
+
+    }
+}
